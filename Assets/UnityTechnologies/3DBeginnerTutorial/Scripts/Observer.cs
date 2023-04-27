9c64b893cf1689abf0a9ff8c3ceb67c4a84859c8
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-04-27 18:22:08 +0100

    11. Complete seu Pré-fabricado Gárgula

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Observer.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/Observer.cs
@@ -38,7 +38,7 @@ public class Observer : MonoBehaviour
 
                 if (raycastHit.collider.transform == player)
                 {
-
+                    gameEnding.CaughtPlayer();
                 }
             }
 
