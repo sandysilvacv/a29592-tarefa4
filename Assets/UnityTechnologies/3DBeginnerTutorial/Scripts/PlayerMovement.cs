ac3649fb8dc2474398547882f529a64a0f451b4c
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-04-27 22:26:21 +0100

    13. Atualize seu script PlayerMovement para reproduzir áudio

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
+
     Animator m_Animator;
     Rigidbody m_Rigidbody;
-
+    AudioSource m_AudioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
-    // Start is called before the first frame update
+   
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -30,6 +31,18 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
