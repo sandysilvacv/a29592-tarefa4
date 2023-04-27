215edf523059bb6484f352495c645ccb67b6004c
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-04-27 12:55:52 +0100

    9. Crie um novo script e 10. Inicie o script GameEnding
    um método de atualização
    adicionou dois métodos, OnTriggerEnter e Update. Quando OnTriggerEnter é chamado, os computadores verificam se o Collider que entrou no Trigger pertence ao personagem do jogador. A atualização é chamada a cada quadro e verifica se o personagem do jogador está na saída. Se o personagem do jogador estiver na saída, ele cairá no bloco de código da instrução if; caso contrário, não faz nada.
    código da instrução if

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameEnding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
     }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+
+    }
+
 }
+
